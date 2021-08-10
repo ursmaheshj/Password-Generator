e9a16d9339e259c8bdecedf8938c990206e6731a
Mahesh Jadhav <omkar28399@gmail.com>
2021-08-10 23:35:57 +0530

    Project tracked

---
+++ generator/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import random
+def home(request):
+    return render(request, 'generator/home.html')
+def about(request):
+    return render(request, 'generator/about.html')
+def password(request):
+    password=''
+    char=list('abcdefghijklmnopqrstuvwxyz')
+    if request.GET.get('numbers'):
+        char.extend(list('0123456789'))
+    if request.GET.get('uppercase'):
+        char.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+    if request.GET.get('specials'):
+        char.extend(list('!@#$%^&*'))
+    len=int(request.GET.get('length'))
+    for i in range(len):
+        password+=random.choice(char)
+    return render(request, 'generator/password.html',{'pass':password})
