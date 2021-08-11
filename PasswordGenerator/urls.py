ae03f9325cbab925404933c549fc6978c3af52ab
Mahesh Jadhav <omkar28399@gmail.com>
2021-08-11 22:34:46 +0530

    URL token for home view updated

--- PasswordGenerator/urls.py
+++ PasswordGenerator/urls.py
@@ -17,7 +17,7 @@ from generator import views
 from django.urls import path
 
 urlpatterns = [
-    path('home/', views.home,name='home'),
+    path('', views.home,name='home'),
     path('password/',views.password,name='password'),
     path('about/',views.about,name='about'),
 ]
